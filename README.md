8d4b2a4df6e3e64821cc97ce078ce87338ee8179
Charles Pierse <charlespierse@gmail.com>
2020-11-15 19:04:14 +0000

    Add setup.py

--- README.md
+++ README.md
@@ -6,3 +6,11 @@
 
 [![build](https://circleci.com/gh/cdpierse/pyinfer.svg?style=shield&circle-token=2c8717a6df1a49242ab0f694f8ab4bb58d376c91)](https://app.circleci.com/pipelines/github/cdpierse/pyinfer)
 <img src="./test/static/coverage.svg">
+
+Pyinfer is a model agnostic Python utility tool for ML developers and researchers to benchmark model inference statistics. 
+
+## Installation
+
+```python
+pip install pyinfer
+```
---
+++ setup.py
@@ -0,0 +1,34 @@
+from distutils.core import setup
+
+from setuptools import find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name = 'pyinfer',
+    packages = find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests", "examples", "docs", "out", "dist","media","test"]),
+    version = '0.0.1',
+    license='Apache-2.0',
+    description = 'Pyinfer is a model agnostic Python utility tool for ML developers and researchers to benchmark model inference statistics.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author = 'Charles Pierse',
+    author_email = 'charlespierse@gmail.com',
+    url = 'https://github.com/cdpierse/pyinfer',
+    keywords = ['machine learning', 'inference', 'benchmark', 'model inference', 'model serving'],
+    install_requires=[
+        'tabulate>=0.8.7',
+        'psutil>=5.7.3'
+    ],
+    extras_require={
+        "Plotting": ["matplotlib>=3.3.2"],
+        "all": ["matplotlib>=3.3.2"]
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
